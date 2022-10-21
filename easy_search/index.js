95d25f91103458270a94304c50a7029307ab3e97
zzall <1809642355@qq.com>
2022-10-21 15:03:50 +0800

    feat:add easy search => https://plugins.jetbrains.com/ search

--- easy_search/index.js
+++ easy_search/index.js
@@ -2,7 +2,7 @@
 // @name:zh-CN   快捷搜索-开发者搜索：覆盖掘金、react docs、google API、vscode插件市场搜索搜索、菜鸟搜索、docker hub、淘宝镜像官网、华为云镜像官网、jenkins、npmjs、mdn、ant.mobile.design、bilibili、github、prettier.io、等基本所有开发者常用网址
 // @name         快捷搜索-开发者搜索：掘金、react、google API、vscode插件市场、菜鸟搜索、docker、淘宝、华为云镜像官网、npmjs、mdn、antd、bilib、github等开发者常用网址
 // @namespace    http://tampermonkey.net/
-// @version      3.18.4
+// @version      3.19.4
 // @description  google translate、mobile.ant.mobile、掘金、npmjs、bilibibli、bootstracpCDN、splunk、google API 快捷搜索，更多快捷搜索
 // @license      MIT
 // @author       zzailianlian
@@ -39,6 +39,7 @@
 // @match        *://greasyfork.org/*
 // @match        *://www.expressjs.com.cn/*
 // @match        *://confluence.babytree-inc.com/*
+// @match        *://plugins.jetbrains.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=npmjs.com
 // @grant        none
 // ==/UserScript==
@@ -121,6 +122,7 @@
     'www.bootcdn.cn': {
       searchSelectorStr: '.search-wraper input[type=text]',
     }, // bootcdn 搜索
+    'plugins.jetbrains.com':'input',
     'developer.chrome.com': '.search-box__input', // 谷歌api文档 搜索
     'cn.vuejs.org': '#search-query-nav', // vue官方文档 搜索
     'cli.vuejs.org': '#algolia-search-input', // vue-cli文档 搜索
