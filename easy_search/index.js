a6018c4bf9c39340e00dc443afed4e727d133d48
zzall <1809642355@qq.com>
2022-10-27 17:06:50 +0800

    feat: add easy_search code.visualstudio search

--- easy_search/index.js
+++ easy_search/index.js
@@ -2,7 +2,7 @@
 // @name:zh-CN   快捷搜索-开发者搜索：覆盖掘金、react docs、google API、vscode插件市场搜索搜索、菜鸟搜索、docker hub、淘宝镜像官网、华为云镜像官网、jenkins、npmjs、mdn、ant.mobile.design、bilibili、github、prettier.io、等基本所有开发者常用网址
 // @name         快捷搜索-开发者搜索：掘金、react、google API、vscode插件市场、菜鸟搜索、docker、淘宝、华为云镜像官网、npmjs、mdn、antd、bilib、github等开发者常用网址
 // @namespace    http://tampermonkey.net/
-// @version      3.19.6
+// @version      3.20.6
 // @description  google translate、mobile.ant.mobile、掘金、npmjs、bilibibli、bootstracpCDN、splunk、google API 快捷搜索，更多快捷搜索
 // @license      MIT
 // @updateURL    https://raw.githubusercontent.com/zzall/temperMonkey/master/easy_search/index.js
@@ -14,6 +14,7 @@
 // @match        https://search.bilibili.com/*
 // @match        http://splunk.ali.plt.babytree-inc.com/*
 // @match        http://developer.chrome.com/*
+// @match        *://code.visualstudio.com/*
 // @match        *://juejin.cn/*
 // @match        *://juejin.im/*
 // @match        *://*.vuejs.org/*
@@ -45,8 +46,8 @@
 // @grant        none
 // ==/UserScript==
 
-; (function () {
-  'use strict'
+(function () {
+  'use strict';
   /**
    * config是一个基础对象
    * key 为 要匹配的url
@@ -93,37 +94,38 @@
     'mirrors.huaweicloud.com': {
       searchSelectorStr: '.devui-select-filter-input',
       preFocusEvent(target) {
-        console.log('target', target)
-        target && (target.style.display = 'block')
-      }
+        console.log('target', target);
+        target && (target.style.display = 'block');
+      },
     },
-    'hub.docker.com': 'input',
-    'confluence.babytree-inc.com': 'input',
-    'www.expressjs.com.cn': 'input',
-    'greasyfork.org': 'input[type=search]',
+    'hub.docker.com': 'input', // docker hub 市场搜索
+    'code.visualstudio.com': 'input', // vscode api 搜索
+    'confluence.babytree-inc.com': 'input', // confluence 搜索
+    'www.expressjs.com.cn': 'input', // express 搜索
+    'greasyfork.org': 'input[type=search]', // greasyfork 搜索
     'www.runoob.com': '#s', // 菜鸟搜索
-    'www.typescriptlang.org': 'input',
-    'jenkins3.plt.babytree-inc.com': 'input',
-    'npmmirror.com': 'input',
+    'www.typescriptlang.org': 'input', // ts官网
+    'jenkins3.plt.babytree-inc.com': 'input', // jenins搜索
+    'npmmirror.com': 'input', //npm镜像官网
     '.plt.babytree-inc.com': '.main-search__input',
     'www.npmjs.com': {
       keyCode: 71,
       metaKey: true,
       searchSelectorStr: "input[type='search']",
       isESCblur: true,
-      cb: () => { },
+      cb: () => {},
     }, // npmjs 搜索
     'docs.npmjs.com': {
       keyCode: 71,
       metaKey: true,
       searchSelectorStr: 'input[type=text]',
       isESCblur: true,
-      cb: () => { },
+      cb: () => {},
     }, // npmjs文档 搜索
     'www.bootcdn.cn': {
       searchSelectorStr: '.search-wraper input[type=text]',
     }, // bootcdn 搜索
-    'plugins.jetbrains.com':'input',
+    'plugins.jetbrains.com': 'input', // jetbrains的插件市场搜索
     'developer.chrome.com': '.search-box__input', // 谷歌api文档 搜索
     'cn.vuejs.org': '#search-query-nav', // vue官方文档 搜索
     'cli.vuejs.org': '#algolia-search-input', // vue-cli文档 搜索
@@ -131,12 +133,12 @@
     juejin: {
       searchSelectorStr: 'input[type="search"]',
       cb: () => {
-        ;[...document.querySelector('.main-header').classList].includes(
+        [...document.querySelector('.main-header').classList].includes(
           'visible'
         )
           ? null
-          : document.querySelector('.main-header').classList.add('visible')
-        document.querySelector('input[type="search"]').focus()
+          : document.querySelector('.main-header').classList.add('visible');
+        document.querySelector('input[type="search"]').focus();
       },
     }, // 掘金搜索
     'marketplace.visualstudio.com': '.search-input', // vscode 市场搜索
@@ -151,7 +153,7 @@
             .querySelector(
               'body > div.shared-page > div.main-section-body > div > div.section-padded.section-header > div.search-bar-wrapper.shared-searchbar > form > table > tbody > tr > td.search-button > a'
             )
-            .click()
+            .click();
         },
       }, // splunk搜索按钮点击
     ],
@@ -159,35 +161,35 @@
       cb: () => {
         const inputEl =
           document.querySelector('#search-input') ||
-          document.querySelector('#dashboard_search')
+          document.querySelector('#dashboard_search');
         if (inputEl) {
-          inputEl.focus()
+          inputEl.focus();
         } else {
-          const homeEl = document.querySelector('.pure-menu-link')
-          homeEl.click()
+          const homeEl = document.querySelector('.pure-menu-link');
+          homeEl.click();
           setTimeout(() => {
             const inputEl =
               document.querySelector('#search-input') ||
-              document.querySelector('#dashboard_search')
-            inputEl.focus()
-          }, 500)
+              document.querySelector('#dashboard_search');
+            inputEl.focus();
+          }, 500);
         }
       },
       escCb() {
         const inputEl =
           document.querySelector('#search-input') ||
-          document.querySelector('#dashboard_search')
+          document.querySelector('#dashboard_search');
         if (inputEl) {
-          inputEl.blur()
+          inputEl.blur();
         } else {
-          const homeEl = document.querySelector('.pure-menu-link')
-          homeEl.click()
+          const homeEl = document.querySelector('.pure-menu-link');
+          homeEl.click();
           setTimeout(() => {
             const inputEl =
               document.querySelector('#search-input') ||
-              document.querySelector('#dashboard_search')
-            inputEl.blur()
-          }, 500)
+              document.querySelector('#dashboard_search');
+            inputEl.blur();
+          }, 500);
         }
       },
     }, // splunk搜索
@@ -196,7 +198,10 @@
     react: '#algolia-doc-search', // react 官网搜索
     'eslint.org': '#search', // eslint.org 搜索
     prettier: '#search_input_react', // prettier.io 搜索
-    'github.com': ['.application-main input[type=search]','.application-main input[type=text]'], // github search your repositories
+    'github.com': [
+      '.application-main input[type=search]',
+      '.application-main input[type=text]',
+    ], // github search your repositories
     // 'github.com': 'input.js-your-repositories-search', // github search your repositories
     'docs.github.com': 'input[type=search]', // github search your repositories
     // 该写翻译中英文了
@@ -205,19 +210,19 @@
       {
         keyCode: 70,
         cb() {
-          const enStr = 'sl=en'
-          const cnStr = 'sl=zh-CN'
-          const isEn = window.location.href.includes(enStr)
+          const enStr = 'sl=en';
+          const cnStr = 'sl=zh-CN';
+          const isEn = window.location.href.includes(enStr);
           var str = isEn
             ? getUrlWithObj({
-              sl: 'zh-CN',
-              tl: 'en',
-            })
+                sl: 'zh-CN',
+                tl: 'en',
+              })
             : getUrlWithObj({
-              sl: 'en',
-              tl: 'zh-CN',
-            })
-          window.location.href = str
+                sl: 'en',
+                tl: 'zh-CN',
+              });
+          window.location.href = str;
         }, // 切换中英文
       },
     ],
@@ -230,7 +235,7 @@
             .querySelector(
               'body > div.container-wrap.mb-g > div.vip-right > div.right-box > div.am-titlebar.am-titlebar-default.am-no-layout > nav > form > button:nth-child(9)'
             )
-            .click()
+            .click();
         },
       }, // 埋点系统搜索按钮点击
       // {
@@ -241,7 +246,7 @@
       //   },
       // }, //  meta（cmd/windows按键） + c  点击复制后直接调起筛选
     ],
-  }
+  };
 
   // 基于config生成指定监听事件
   const generateMain = () => {
@@ -251,12 +256,12 @@
       keyCode: 71,
       isPreventDefault: true,
       searchSelectorStr: '',
-      cb: () => { },
-      escCb: () => { },
-      preFocusEvent: () => { },
-    }
+      cb: () => {},
+      escCb: () => {},
+      preFocusEvent: () => {},
+    };
     document.onkeydown = function (event) {
-      var e = event || window.event
+      var e = event || window.event;
       const generateMainChild = (href, val) => {
         const {
           metaKey,
@@ -267,86 +272,86 @@
           cb,
           escCb,
           preFocusEvent,
-        } = { ...defaultOpts, ...val }
-        const isMetaKey = e.metaKey && metaKey
-        const isEqualKeyCode = e.keyCode == keyCode
+        } = { ...defaultOpts, ...val };
+        const isMetaKey = e.metaKey && metaKey;
+        const isEqualKeyCode = e.keyCode == keyCode;
         if (isMetaKey && isEqualKeyCode) {
-          isPreventDefault && e.preventDefault()
+          isPreventDefault && e.preventDefault();
           if (window.location.origin.includes(href)) {
             if (searchSelectorStr) {
               const searchCommonDom =
                 searchSelectorStr instanceof Array
                   ? searchSelectorStr
-                    .map(selector => document.querySelector(selector))
-                    .find(Boolean)
-                  : document.querySelector(searchSelectorStr)
-              console.log('searchCommonDom', searchCommonDom)
-              preFocusEvent && preFocusEvent(searchCommonDom)
-              searchCommonDom && searchCommonDom.focus()
-              return cb && cb(searchCommonDom)
+                      .map(selector => document.querySelector(selector))
+                      .find(Boolean)
+                  : document.querySelector(searchSelectorStr);
+              console.log('searchCommonDom', searchCommonDom);
+              preFocusEvent && preFocusEvent(searchCommonDom);
+              searchCommonDom && searchCommonDom.focus();
+              return cb && cb(searchCommonDom);
             }
-            cb && cb()
+            cb && cb();
           }
         }
         if (isESCblur && e.keyCode === 27) {
-          isPreventDefault && e.preventDefault()
+          isPreventDefault && e.preventDefault();
           if (searchSelectorStr) {
-            const searchCommonDom = document.querySelector(searchSelectorStr)
-            searchCommonDom && searchCommonDom.blur()
+            const searchCommonDom = document.querySelector(searchSelectorStr);
+            searchCommonDom && searchCommonDom.blur();
           }
-          escCb && escCb()
+          escCb && escCb();
         }
-      }
+      };
       Object.entries(config).map(item => {
-        let [href, vals] = item
-        if (!window.location.origin.includes(href)) return
-        console.log('href',href,window.location.origin.includes(href))
+        let [href, vals] = item;
+        if (!window.location.origin.includes(href)) return;
+        console.log('href', href, window.location.origin.includes(href));
         if (typeof vals === 'string') {
-          return generateMainChild(href, { searchSelectorStr: vals })
+          return generateMainChild(href, { searchSelectorStr: vals });
         }
         if (vals instanceof Array) {
           return vals.map(valsItem => {
             if (typeof valsItem === 'string' || valsItem instanceof Array) {
               generateMainChild(href, {
                 searchSelectorStr: valsItem,
-              })
+              });
             } else {
-              generateMainChild(href, valsItem)
+              generateMainChild(href, valsItem);
             }
-          })
+          });
         }
-        generateMainChild(href, vals)
-      })
-    }
-  }
+        generateMainChild(href, vals);
+      });
+    };
+  };
 
-  generateMain()
+  generateMain();
 
   // 获取url的params object
   const getUrlParams = () => {
-    var obj = {}
+    var obj = {};
     window.location.search
       .slice(1)
       .split('&')
       .map(item => {
         if (item.split('=')) {
-          const [key, value] = item.split('=')
+          const [key, value] = item.split('=');
           if (key && value) {
-            obj[key] = value
+            obj[key] = value;
           }
         }
-      })
-    return obj
-  }
+      });
+    return obj;
+  };
   // 将新的参数已object的形式拼接到url上，如果key值相同，则后者覆盖前者
   const getUrlWithObj = obj => {
-    const originHrefBase = window.location.href.split('?')[0]
-    const originParamsObj = getUrlParams()
-    const newObj = { ...originParamsObj, ...obj }
+    const originHrefBase = window.location.href.split('?')[0];
+    const originParamsObj = getUrlParams();
+    const newObj = { ...originParamsObj, ...obj };
     const searchStr = Object.entries(newObj).reduce(
       (pre, cur) => pre + `${cur[0]}=${cur[1]}&`,
       ''
-    )
-    return `${originHrefBase}?${searchStr.slice(0, -1)}`
-  }
-})()
+    );
+    return `${originHrefBase}?${searchStr.slice(0, -1)}`;
+  };
+})();
