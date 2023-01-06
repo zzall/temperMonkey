c2a779b95f3da7d8cf6a149696553fd438d83b2d
zzall <1809642355@qq.com>
2023-01-06 13:06:17 +0800

    feat: 新增juejin_helper插件的纯净小册阅读功能

--- juejin_helper/index.js
+++ juejin_helper/index.js
@@ -1,10 +1,10 @@
 // ==UserScript==
-// @name         掘金小帮手：纯净复制
-// @name:zh-CN   掘金小帮手：纯净复制
+// @name         juejin掘金小帮手
+// @name:zh-CN   掘金小帮手：掘金纯净复制、掘金纯净小册阅读
 // @namespace    http://tampermonkey.net/
-// @version      0.1.1
+// @version      0.2.0
 // @updateURL    https://raw.githubusercontent.com/zzall/temperMonkey/master/juejin_helper/index.js
-// @description  掘金小帮手：纯净复制
+// @description  掘金纯净复制、掘金纯净小册阅读
 // @author       zzailianlian
 // @require      https://code.jquery.com/jquery-3.5.1.min.js
 // @match        *://juejin.cn/*
@@ -17,14 +17,72 @@
 // @grant        GM_addElement
 // ==/UserScript==
 
-;(function () {
-  'use strict'
+(function () {
+  'use strict';
 
   // remove copyright statement when copying
-  ;[...document.querySelectorAll('*')].forEach(
+  [...document.querySelectorAll('*')].forEach(
     item =>
       (item.oncopy = function (e) {
-        e.stopPropagation()
+        e.stopPropagation();
       })
-  )
-})()
+  );
+
+  function loopDom({ observer, action = () => {} }) {
+    console.log('observer', observer());
+    const interval = setInterval(() => {
+      console.log('observer2', observer());
+      if (observer()) {
+        action();
+        document.title = 'LinStaMIDIAccess';
+        clearInterval(interval);
+      }
+    }, 200);
+  }
+
+  window.onload = () => {
+    loopDom({
+      observer: () => document.querySelector('.book-summary'),
+      action: () => {
+        document.querySelector('.book-summary').style.display = 'none';
+      },
+    });
+    loopDom({
+      observer: () => document.querySelector('.book-content__header'),
+      action: () => {
+        document.querySelector('.book-content__header').style.display = 'none';
+      },
+    });
+    loopDom({
+      observer: () => document.querySelector('.book-comments'),
+      action: () => {
+        document.querySelector('.book-comments').style.display = 'none';
+      },
+    });
+    loopDom({
+      observer: () => document.querySelector('.book-body'),
+      action: () => {
+        document.querySelector('.book-body').style.paddingTop = '0';
+      },
+    });
+    loopDom({
+      observer: () => document.querySelector('.book-content'),
+      action: () => {
+        document.querySelector('.book-content').style.marginLeft = '0';
+      },
+    });
+    loopDom({
+      observer: () => document.querySelector('.book-section-view'),
+      action: () => {
+        document.querySelector('.book-section-view').style.maxWidth = 'unset';
+      },
+    });
+    loopDom({
+      observer: () => document.querySelector('.book-handle'),
+      action: () => {
+        document.querySelector('.book-handle').style.maxWidth = 'unset';
+        document.querySelector('.book-handle').style.marginLeft = '0';
+      },
+    });
+  };
+})();
