5667c9cf41532114c95e609040956b0a093108ad
zzall <1809642355@qq.com>
2023-02-02 16:51:51 +0800

    fix: juejin_helper pamphlethelper immerse reading feature;

--- juejin_helper/index.js
+++ juejin_helper/index.js
@@ -2,7 +2,7 @@
 // @name         juejin掘金小帮手
 // @name:zh-CN   掘金小帮手：掘金纯净复制、掘金纯净小册阅读
 // @namespace    http://tampermonkey.net/
-// @version      0.2.0
+// @version      0.2.1
 // @updateURL    https://raw.githubusercontent.com/zzall/temperMonkey/master/juejin_helper/index.js
 // @description  掘金纯净复制、掘金纯净小册阅读
 // @author       zzailianlian
@@ -20,7 +20,7 @@
 (function () {
   'use strict';
 
-  // remove copyright statement when copying
+  // 复制去除后缀
   [...document.querySelectorAll('*')].forEach(
     item =>
       (item.oncopy = function (e) {
@@ -40,49 +40,85 @@
     }, 200);
   }
 
+  // 沉浸式小册阅读
+  const openJuejinPamphlethelper = () => {
+    if (/juejin\.[cnim]{2}.+\/section/.test(window.location.href)) {
+      loopDom({
+        observer: () => document.querySelector('.book-summary'),
+        action: () => {
+          console.log('触发book-summary的action', document.querySelector('.book-summary'));
+          document.querySelector('.book-summary').style.display = 'none';
+          console.log(
+            '触发book-summary的action',
+            document.querySelector('.book-summary'),
+            document.querySelector('.book-summary').style.display
+          );
+        },
+      });
+      loopDom({
+        observer: () => document.querySelector('.book-content__header'),
+        action: () => {
+          document.querySelector('.book-content__header').style.display = 'none';
+        },
+      });
+      loopDom({
+        observer: () => document.querySelector('.book-comments'),
+        action: () => {
+          document.querySelector('.book-comments').style.display = 'none';
+        },
+      });
+      loopDom({
+        observer: () => document.querySelector('.book-body'),
+        action: () => {
+          document.querySelector('.book-body').style.paddingTop = '0';
+        },
+      });
+      loopDom({
+        observer: () => document.querySelector('.book-content'),
+        action: () => {
+          document.querySelector('.book-content').style.marginLeft = '0';
+        },
+      });
+      loopDom({
+        observer: () => document.querySelector('.book-section-view'),
+        action: () => {
+          document.querySelector('.book-section-view').style.maxWidth = 'unset';
+        },
+      });
+      loopDom({
+        observer: () => document.querySelector('.book-handle'),
+        action: () => {
+          document.querySelector('.book-handle').style.maxWidth = 'unset';
+          document.querySelector('.book-handle').style.marginLeft = '0';
+        },
+      });
+    }
+  };
+
   window.onload = () => {
-    loopDom({
-      observer: () => document.querySelector('.book-summary'),
-      action: () => {
-        document.querySelector('.book-summary').style.display = 'none';
-      },
+    var _wr = function (type) {
+      var orig = history[type];
+      return function () {
+        var rv = orig.apply(this, arguments);
+        var e = new Event(type);
+        e.arguments = arguments;
+        window.dispatchEvent(e);
+        return rv;
+      };
+    };
+
+    window.addEventListener('replaceState', function (e) {
+      console.log('监听自定义replaceState');
+      openJuejinPamphlethelper();
     });
-    loopDom({
-      observer: () => document.querySelector('.book-content__header'),
-      action: () => {
-        document.querySelector('.book-content__header').style.display = 'none';
-      },
+    window.addEventListener('pushState', function (e) {
+      console.log('监听自定义pushState');
+      openJuejinPamphlethelper();
     });
-    loopDom({
-      observer: () => document.querySelector('.book-comments'),
-      action: () => {
-        document.querySelector('.book-comments').style.display = 'none';
-      },
-    });
-    loopDom({
-      observer: () => document.querySelector('.book-body'),
-      action: () => {
-        document.querySelector('.book-body').style.paddingTop = '0';
-      },
-    });
-    loopDom({
-      observer: () => document.querySelector('.book-content'),
-      action: () => {
-        document.querySelector('.book-content').style.marginLeft = '0';
-      },
-    });
-    loopDom({
-      observer: () => document.querySelector('.book-section-view'),
-      action: () => {
-        document.querySelector('.book-section-view').style.maxWidth = 'unset';
-      },
-    });
-    loopDom({
-      observer: () => document.querySelector('.book-handle'),
-      action: () => {
-        document.querySelector('.book-handle').style.maxWidth = 'unset';
-        document.querySelector('.book-handle').style.marginLeft = '0';
-      },
-    });
+
+    history.pushState = _wr('pushState');
+    history.replaceState = _wr('replaceState');
+
+    openJuejinPamphlethelper();
   };
 })();
