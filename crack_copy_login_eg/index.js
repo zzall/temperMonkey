bdf9d0a7130a3a0258d73f0a9d59351931758861
zzall <1809642355@qq.com>
2023-02-06 14:22:57 +0800

    feat: add crack_copy_login_eg jianshu

--- crack_copy_login_eg/index.js
+++ crack_copy_login_eg/index.js
@@ -2,26 +2,27 @@
 // @name         破解禁止登陆，破解禁止复制
 // @name:zh-CN   破解禁止登陆，破解禁止复制
 // @namespace    http://tampermonkey.net/
-// @version      0.1.1
+// @version      0.2.0
 // @description  破解禁止登陆，破解禁止复制
 // @author       zzailianlian
 // @updateURL    https://raw.githubusercontent.com/zzall/temperMonkey/master/crack_copy_login_eg/index.js
 // @require      https://code.jquery.com/jquery-3.5.1.min.js
 // @match        *://*.360doc.com/*
 // @match        *://juejin.im/*
+// @match        *://*.jianshu.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=meitun-test.com
 // @license      MIT
 // @run-at       document-idle
 // ==/UserScript==
 
-; (function () {
-  'use strict'
+(function () {
+  'use strict';
 
-    // remove copyright statement when copying
-    ;[...document.querySelectorAll('*')].forEach(
-      item =>
+  // remove copyright statement when copying
+  [...document.querySelectorAll('*')].forEach(
+    item =>
       (item.oncopy = function (e) {
-        e.stopPropagation()
+        e.stopPropagation();
       })
-    )
-})()
+  );
+})();
