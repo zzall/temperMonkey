cb95cb417bb4ad502594bd12df9dcd1605c7e543
zzall <1809642355@qq.com>
2023-04-21 16:12:52 +0800

    feat: desc

--- crack_copy_login_eg/index.js
+++ crack_copy_login_eg/index.js
@@ -1,8 +1,12 @@
 // ==UserScript==
-// @name         破解复制登录、去除复制后缀、网页全局可选
-// @name:zh-CN   可以破解禁止登录和复制的限制。它可以让你在网页上自由复制和粘贴，而不会受到版权声明的限制。
+// @name        Cracking replication login, removing replication suffixes, web page global Optional
+// @name:en     Cracking replication login, removing replication suffixes, web page global Optional
+// @name:zh-CN   破解复制登录、去除复制后缀、网页全局可选
+// @description   You can circumvent the restrictions that prohibit login and replication. It allows you to copy and paste freely on web pages without being restricted by copyright notices.
+// @description:en     You can circumvent the restrictions that prohibit login and replication. It allows you to copy and paste freely on web pages without being restricted by copyright notices.
+// @description:zh-CN  可以破解禁止登录和复制的限制。它可以让你在网页上自由复制和粘贴，而不会受到版权声明的限制。
 // @namespace    http://tampermonkey.net/
-// @version      0.3.1
+// @version      0.4.0
 // @description  破解禁止登陆，破解禁止复制
 // @author       zzailianlian
 // @updateURL    https://raw.githubusercontent.com/zzall/temperMonkey/master/crack_copy_login_eg/index.js
@@ -14,6 +18,16 @@
 
 (function () {
   'use strict';
+
+  /** 针对钉钉文档不让复制的问题，考虑有以下解决办法
+   * 1. 通过charles代理重写接口绕开权限校验
+   * 2. 通过浏览器打开钉钉文档，然后通过谷歌插件重写权限接口，绕开权限校验
+   * 3. 本地把网页保存到本地文件 ， 这个是ok的
+   * 4. 重写复制功能
+   * 5. 选中时触发自定义复制功能
+   * 6. 筛选dom元素通过innerText获取内容并且拼接字符串
+   * 7. 通过截图文件截长图
+   */
 
   [...document.querySelectorAll('*')].forEach(item => {
     // 复制时删除版权声明，打破复制限制
