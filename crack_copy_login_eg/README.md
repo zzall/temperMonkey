3f469365793723a18d4bf5cc862b2e5325abc1bc
zzall <1809642355@qq.com>
2023-04-25 12:24:43 +0800

    feat: copy

--- crack_copy_login_eg/README.md
+++ crack_copy_login_eg/README.md
@@ -1,16 +1,26 @@
-为了方便用户使用，以下是这个油猴脚本的介绍文档：
-
-# 破解复制登录、去除复制后缀
+# 破解复制登录、去除复制后缀、网页全局可选、新增code标签代码复制
 
 这是一个油猴脚本，可以破解禁止登录和复制的限制。它可以让你在网页上自由复制和粘贴，而不会受到版权声明的限制。
 
 ## 安装
 
-1. 安装 Tampermonkey 插件
-2. 点击这个链接：https://raw.githubusercontent.com/zzall/temperMonkey/master/crack_copy_login_eg/index.js
-3. 点击 Tampermonkey 的安装按钮
+1. 安装油猴插件
+2. 点击 [安装](https://greasyfork.org/zh-CN/scripts/459509-%E7%A0%B4%E8%A7%A3%E5%A4%8D%E5%88%B6%E7%99%BB%E5%BD%95-%E5%8E%BB%E9%99%A4%E5%A4%8D%E5%88%B6%E5%90%8E%E7%BC%80-%E7%BD%91%E9%A1%B5%E5%85%A8%E5%B1%80%E5%8F%AF%E9%80%89)
+3. github [这里](https://github.com/zzall/temperMonkey/tree/master/crack_copy_login_eg)
+
+## 功能
+
+- 破解复制登录、去除复制后缀、网页全局可选、新增code标签代码复制
 
 ## 使用
 
-安装成功后，该脚本会自动运行。你可以在任何网页上使用它，无需任何额外的设置。
+安装脚本后，打开任意网页即可使用。
+
+## 作者
+
+zzailianlian
+
+## 许可证
+
+MIT
 
--- crack_copy_login_eg/index.js
+++ crack_copy_login_eg/index.js
@@ -1,13 +1,13 @@
 // ==UserScript==
-// @name        破解复制登录、去除复制后缀、网页全局可选
+// @name        破解复制登录、去除复制后缀、网页全局可选、新增code标签代码复制
 // @name:en     Cracking replication login, removing replication suffixes, web page global Optional
-// @name:zh-CN   破解复制登录、去除复制后缀、网页全局可选
+// @name:zh-CN   破解复制登录、去除复制后缀、网页全局可选、新增code标签代码复制
 // @description   可以破解禁止登录和复制的限制。它可以让你在网页上自由复制和粘贴，而不会受到版权声明的限制。
 // @description:en     You can circumvent the restrictions that prohibit login and replication. It allows you to copy and paste freely on web pages without being restricted by copyright notices.
 // @description:zh-CN  可以破解禁止登录和复制的限制。它可以让你在网页上自由复制和粘贴，而不会受到版权声明的限制。
 // @namespace    http://tampermonkey.net/
-// @version      0.6.1
-// @description  破解禁止登陆，破解禁止复制
+// @version      0.7.0
+// @description  破解复制登录、去除复制后缀、网页全局可选、新增code标签代码复制
 // @author       zzailianlian
 // @updateURL    https://raw.githubusercontent.com/zzall/temperMonkey/master/crack_copy_login_eg/index.js
 // @match        *://*/*
@@ -19,17 +19,28 @@
 (function () {
   'use strict';
 
-  /** 针对钉钉文档不让复制的问题，考虑有以下解决办法
-   * 1. 通过charles代理重写接口绕开权限校验
-   * 2. 通过浏览器打开钉钉文档，然后通过谷歌插件重写权限接口，绕开权限校验
-   * 3. 本地把网页保存到本地文件 ， 这个是ok的
-   * 4. 重写复制功能
-   * 5. 选中时触发自定义复制功能
-   * 6. 筛选dom元素通过innerText获取内容并且拼接字符串
-   * 7. 通过截图文件截长图
-   */
-
-  [...document.querySelectorAll('*')].forEach(item => {
+  const initMountHandle = item => {
+    if (!item.tagName) return;
+    if (item.tagName === 'CODE' && !item.innerHTML.includes('new_add_crack_copy_login_eg')) {
+      // 在code区域右上角创建一个固定定位的div框
+      const div = document.createElement('div');
+      div.style.position = 'absolute';
+      div.style.top = '0';
+      div.style.right = '0';
+      div.style.padding = '8px';
+      div.setAttribute('new_add_crack_copy_login_eg', true);
+      // div.style.width = '100px';
+      // div.style.height = '100px';
+      div.style.backgroundColor = 'red';
+      div.style.userSelect = 'none';
+      div.style.cursor = 'pointer';
+      div.innerText = '复制';
+      div.onclick = () => {
+        copyText(item.innerText.slice(0, -2));
+      };
+      item.appendChild(div);
+      item.style.position = 'relative';
+    }
     // 复制时删除版权声明，打破复制限制
     item.oncopy = function (e) {
       e.stopPropagation();
@@ -43,5 +54,70 @@
     );
     // 全局可选择
     item.style.setProperty('user-select', 'auto');
+    //   const removedNodes = mutation.removedNodes;
+    // 处理新增或删除的DOM元素
+  };
+
+  // 获取 body 元素
+  const targetNode = document.querySelector('body');
+
+  // 创建新 observer 对象并传入回调函数
+  const observer = new MutationObserver(mutations => {
+    mutations.forEach(mutation => {
+      if (mutation.type === 'childList') {
+        const addedNodes = mutation.addedNodes;
+
+        if (addedNodes instanceof NodeList) {
+          // const flag = [...addedNodes].every(node => node.getAttribute('new_add_crack_copy_login_eg'))
+          // if (flag) {
+          //   return;
+          // }
+          [...addedNodes].forEach(item => {
+            if (!item.tagName) return;
+            [...item.children].forEach(child => {
+              initMountHandle(child, 'add');
+            });
+          });
+        }
+      }
+    });
   });
+
+  // 配置观察选项（可选择监听属性、子孙节点等）
+  const config = { attributes: false, childList: true, subtree: true, characterData: false };
+
+  // 将配置选项和 callback 函数都传入 observer 实例, 然后开始观察
+  observer.observe(targetNode, config);
+
+  // 停止观察
+  // observer.disconnect();
+
+  function copyText(text) {
+    var textArea = document.createElement('textarea');
+    textArea.value = text;
+    textArea.setAttribute('new_add_crack_copy_login_eg', true);
+    document.body.appendChild(textArea);
+    textArea.select();
+    try {
+      var successful = document.execCommand('copy');
+      var msg = successful ? '已成功复制到剪贴板！' : '无法复制所选文本';
+      console.log(msg);
+    } catch (err) {
+      console.error('无法复制所选文本', err);
+    }
+    document.body.removeChild(textArea);
+  }
+  [...document.querySelectorAll('*')].forEach(item => {
+    initMountHandle(item);
+  });
+
+  /** 针对钉钉文档不让复制的问题，考虑有以下解决办法
+   * 1. 通过charles代理重写接口绕开权限校验
+   * 2. 通过浏览器打开钉钉文档，然后通过谷歌插件重写权限接口，绕开权限校验
+   * 3. 本地把网页保存到本地文件 ， 这个是ok的
+   * 4. 重写复制功能
+   * 5. 选中时触发自定义复制功能
+   * 6. 筛选dom元素通过innerText获取内容并且拼接字符串
+   * 7. 通过截图文件截长图
+   */
 })();
